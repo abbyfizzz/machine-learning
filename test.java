843d9b418b362ec2672a304db82d0ffdeaa47e7a
vasanth <vasanth.amias@gmail.com>
2016-09-30 10:29:18 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+import java.lang.Math.*;
+import java.util.*;
+
+public class test {
+
+    public static void main(String[] args) {
+Scanner in =new Scanner(System.in);
+int t=in.nextInt();
+in.nextInt();
+int c=0; //count
+for(int i=0;i<t;i++)
+{
+	int y =in.nextInt();
+	int x1=in.nextInt();
+	
+}
+System.out.println(c);
+
+}
+}
